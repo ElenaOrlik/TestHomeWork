1cacd26edb53145445779d191d10db6a8716ca23
ElenaO <alionka-09@mail.ru>
2023-05-21 20:24:07 +0500

    выполнено Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (n > m)
+    Console.WriteLine(n);
+else if (n < m)
+    Console.WriteLine(m);
+else
+    Console.WriteLine(m);
