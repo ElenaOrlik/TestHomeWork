48f35bb215ad26c72ef3fcaa7335d7aed6fd1eed
ElenaO <alionka-09@mail.ru>
2023-05-27 14:33:46 +0500

    Завершение ДЗ по второму семинару

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблиись!\nВведите номер дня недели: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n==6|| n==7)
+Console.WriteLine("Это выходной!!!: ");
+else
+Console.WriteLine("Это будни!!!: ");
+    
