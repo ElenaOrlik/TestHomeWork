5a30d852fdb99ec6668893e79b2e6e2728756747
ElenaO <alionka-09@mail.ru>
2023-06-01 01:57:00 +0500

    Завершение ДЗ по 4  семинару 25, 27, 29

---
+++ Task25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+double a = double.Parse(Console.ReadLine()!);
+Console.Write("Введите число В: ");
+double b = double.Parse(Console.ReadLine()!);
+double c = Math.Pow(a, b);
+Console.WriteLine($"Ответ: {a} в степени {b} равен {c}");
---
+++ Task27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 0, m = n;
+    while (n > 0)
+    {
+    i += n % 10;
+    n /= 10;
+    }
+Console.WriteLine($"Сумма цифр в числе {m} равна {i}");
+
