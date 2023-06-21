a8d3c8993e7a333c34038496825a5ea63d93ff03
ElenaO <alionka-09@mail.ru>
2023-06-21 22:16:49 +0500

    Завершение ДЗ по 9  семинару, задача 68 Рекурсия, Функция Аккермана

---
+++ Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿int FunctionA(int a, int b)
+{
+    if (a == 0)
+    {
+        return b + 1;
+    }
+    else if (b == 0 && a > 0)
+    {
+        return FunctionA(a - 1, 1);
+    }
+    else
+    {
+        return (FunctionA(a - 1, FunctionA(a, b - 1)));
+    }
+}
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Ответ: {FunctionA(n, m)}");
