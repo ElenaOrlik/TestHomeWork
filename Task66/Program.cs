e6f90dbbd893d6d53019a2f23a0c174d62629976
ElenaO <alionka-09@mail.ru>
2023-06-21 21:55:32 +0500

    Завершение ДЗ по 9  семинару, задача 66 Рекурсия

---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int summa(int a, int b)
+{
+    if (a == b)
+    return b;
+    else return a + summa(a + 1, b);
+}
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {n} до {m} равна {summa(n, m)}");
