e5bdfcf6a58c666223afd7f6b7effa3f768c09e9
ElenaO <alionka-09@mail.ru>
2023-06-21 21:37:40 +0500

    Завершение ДЗ по 9  семинару, задача 64 Рекурсия

---
+++ Task64/Program.cs
@@ -0,0 +1,14 @@
+﻿string printNumbers(int a, int b)
+{
+    if (a == b)
+        return $" {b}";
+    return $" {b}"+ printNumbers (a, b - 1);
+}
+
+
+Console.Clear();
+int m = 1;
+Console.WriteLine( $"1-ое число равно: {m}");
+Console.Write("Введите 2-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNumbers(m, n));
